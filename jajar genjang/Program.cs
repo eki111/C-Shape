04367dfa117df13bcaf86a45d0b6a733d61b8655
eki111 <ahmadheheh12@gmail.com>
2024-11-26 07:15:41 +0700

    Add files via upload

---
+++ jajar genjang/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+class Program 
+{ 
+ static void Main() 
+ { 
+
+ Console.Write("Masukkan nilai alas jajarggenjang: "); 
+ int alas = Convert.ToInt32(Console.ReadLine()); 
+
+ Console.Write("Masukkan nilai tinggi jajarggenjang: "); 
+ int tinggi = Convert.ToInt32(Console.ReadLine()); 
+ 
+ int hasil = alas *tinggi; 
+
+ Console.WriteLine("Hasil penjumlahan: " + hasil);
+ }
+}
---
+++ pengurangan/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+class Program 
+{ 
+ static void Main() 
+ { 
+
+ Console.Write("Masukkan angka pertama: "); 
+ int angka1 = Convert.ToInt32(Console.ReadLine()); 
+
+ Console.Write("Masukkan angka kedua: "); 
+ int angka2 = Convert.ToInt32(Console.ReadLine()); 
+ 
+ int hasil = angka1 - angka2; 
+
+ Console.WriteLine("Hasil penjumlahan: " + hasil);
+ }
+}
